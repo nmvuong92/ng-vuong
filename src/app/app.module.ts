1220e6dcc867e022e862e4be1070555886f0c469
ng-vượng <nmvuong92@gmail.com>
2018-04-05 15:55:19 +0700

    Yap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HumanComponent } from './human/human.component';
+import { NewsComponent } from './news/news.component';
+import { ProductsComponent } from './products/products.component';
+import { ConfigComponent } from './config/config.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HumanComponent,
+    NewsComponent,
+    ProductsComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule
